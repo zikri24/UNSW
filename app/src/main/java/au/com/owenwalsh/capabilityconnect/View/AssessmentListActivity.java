27bf8ba095674762bf960c14256e63218dd1f64f
owen walsh <owen walsh>
2016-10-17 19:55:51 +1100

    design updates icons

--- app/src/main/java/au/com/owenwalsh/capabilityconnect/View/AssessmentListActivity.java
+++ app/src/main/java/au/com/owenwalsh/capabilityconnect/View/AssessmentListActivity.java
@@ -1,15 +1,26 @@
 package au.com.owenwalsh.capabilityconnect.View;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import au.com.owenwalsh.capabilityconnect.R;
 
-public class AssessmentListActivity extends AppCompatActivity {
+public class AssessmentListActivity extends BaseActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_assessment_list);
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_assessment_list, null, false);
+        drawerLayout.addView(contentView, 0);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
