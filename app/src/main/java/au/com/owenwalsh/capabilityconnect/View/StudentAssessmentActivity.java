1a1dcf7ad4b64964cd18ec722e6f187f743a4bee
owen walsh <owen walsh>
2016-10-23 18:40:10 +1100

    competencies

--- app/src/main/java/au/com/owenwalsh/capabilityconnect/View/StudentAssessmentActivity.java
+++ app/src/main/java/au/com/owenwalsh/capabilityconnect/View/StudentAssessmentActivity.java
@@ -1,13 +1,29 @@
 package au.com.owenwalsh.capabilityconnect.View;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
-public class StudentAssessmentActivity extends AppCompatActivity {
+import au.com.owenwalsh.capabilityconnect.R;
+
+public class StudentAssessmentActivity extends BaseActivity {
+    private static final String STU_ID = "stuID";
+    private RecyclerView recyclerView;
+    private Button submitButton;
+    private TextView emptyView;
+
+    private CompetencyLogic competencyLogic;
+    private ArrayList<Competency> competencies;
+    private StudentCompetencyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_assessment);
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_student_assessment, null, false);
+        drawerLayout.addView(contentView, 0);
     }
 }
