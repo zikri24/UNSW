094dfe1f49d9ce8e0c846ee95f578287331fa130
owen walsh <owen walsh>
2016-10-24 00:45:26 +1100

    need sleep

--- app/src/main/java/au/com/owenwalsh/capabilityconnect/View/SplashActivity.java
+++ app/src/main/java/au/com/owenwalsh/capabilityconnect/View/SplashActivity.java
@@ -21,7 +21,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent i=new Intent(SplashActivity.this, MainActivity.class);
+                Intent i=new Intent(SplashActivity.this, WeeksListActivity.class);
                 startActivity(i);
                 finish();
             }
